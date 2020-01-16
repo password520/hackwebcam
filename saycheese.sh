a0e1d1d08e2d10a93bfc06057b62788ae65d5080
HACKGM <48313924+HACKGM@users.noreply.github.com>
2020-01-16 16:13:49 +0330

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # SayCheese v1.0
-# coded by: github.com/thelinuxchoice/saycheese
+# coded by: github.com/HACKGM/hackwebcam
 # If you use any part from this code, giving me the credits. Read the Lincense!
 
 trap 'printf "\n";stop' 2
