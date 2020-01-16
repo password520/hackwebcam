f9c2d15736581c811251f8b82feece5d77936e7e
HACKGM <48313924+HACKGM@users.noreply.github.com>
2020-01-16 16:41:40 +0330

    Update saycheese.sh

--- saycheese.sh
+++ saycheese.sh
@@ -9,7 +9,7 @@ banner() {
 
 
 
-
+printf "\e[1;92m     \e[0m\e
 
 printf "\e[1;92m 𝙃𝘼𝘾𝙆 𝙒𝙀𝘽𝘾𝘼𝙈\e[0m\e   \e[0m\n"
 printf "\e[1;92m Canal telegram: T.me/HACKGM\e[0m                                 \n"
